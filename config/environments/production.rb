20e9783bf91f20ec68f59a66e38820458cf89adf
Erika <ejderas2@gmail.com>
2018-08-31 23:27:37 +0000

    Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,17 @@
 Rails.application.configure do
+
+
+  ActionMailer: :Base.smtp.settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
+
+
   config.action_mailer.default_url_options = { host: 'nomster-erika-deras.herokuapp.com'}
   
     
